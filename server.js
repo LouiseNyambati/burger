42e0ca9620f3c4704c15f5b0e27998181e52e593
Louise Nyambati <louisen1993@gmail.com>
2017-05-03 19:38:29 -0500

    edited schema

---
+++ server.js
@@ -0,0 +1,5 @@
+var express = require("express");
+var methodOverride = require("method-override");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var mysql = require("mysql");
