0b52eedbaa68cc6865b454f9ef56ca8a41eb1590
Louise Nyambati <louisen1993@gmail.com>
2017-05-05 17:26:04 -0500

    server.js done

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
 var express = require("express");
-var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 var exphbs = require("express-handlebars");
-var mysql = require("mysql");
+
+var port = 3000;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + "/public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes 
+var routes = require("./controllers/burgers_Controller.js");
+
+app.use("/", routes);
+
+app.listen(port);
