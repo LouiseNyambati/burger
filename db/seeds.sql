3f28300d73e9953a74e2db2f970c33810d681976
Louise Nyambati <louisen1993@gmail.com>
2017-05-05 17:50:29 -0500

    edited seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,5 @@
-
-INSERT INTO burgers(burger_name) VALUES ('Jersey Burger');
 INSERT INTO burgers(burger_name) VALUES ('Buffalo Chicken Burger');
-INSERT INTO burgers(burger_name) VALUES ('Veggie Burger');
 INSERT INTO burgers(burger_name) VALUES ('Bacon Cheeseburger');
-INSERT INTO burgers(burger_name) VALUES ('Plain Old Hamburger');
+INSERT INTO burgers(burger_name) VALUES ('Chicken Avocado Burger');
+INSERT INTO burgers(burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers(burger_name) VALUES ('Hamburger');
