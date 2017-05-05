bbbde8ea42296c09aa0525e61ab5d6b9628d5e44
Louise Nyambati <louisen1993@gmail.com>
2017-05-05 18:25:12 -0500

    finishing up

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,3 @@
-INSERT INTO burgers(burger_name) VALUES ('Buffalo Chicken Burger');
 INSERT INTO burgers(burger_name) VALUES ('Bacon Cheeseburger');
 INSERT INTO burgers(burger_name) VALUES ('Chicken Avocado Burger');
-INSERT INTO burgers(burger_name) VALUES ('Veggie Burger');
 INSERT INTO burgers(burger_name) VALUES ('Hamburger');
